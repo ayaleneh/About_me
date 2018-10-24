8dda18b342d5681794d775552a9f3ec2a7240d9c
ayaleneh <getaunayaleneh@gmail.com>
2018-10-24 05:38:20 -0400

    adding html,css and and javascript file to the master branch

---
+++ main.js
@@ -0,0 +1,21 @@
+function handler(){
+  let name;
+ name= prompt("hello can you enter your name?");
+ alert("hello "+name+ " you can put any answer that you want");
+ let m_name=prompt("Do you remeber my name if so write it?"+name);
+ if(m_name==="ayaleneh")
+ {
+   alert("great job! go ahead for next question")
+ }
+ else{
+   alert("you make amistake try the next");
+ }
+ let m_age=prompt("guess my age "+name);
+ if(m_age===22){
+   alert("great..go ahead for the next question");
+ }
+ else{
+   alert("nop..try the next one");
+ }
+ 
+}
