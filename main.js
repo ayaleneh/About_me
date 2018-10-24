cdc4eafce91d1fd78bb93afb568b06fa2e00bd6d
ayaleneh <getaunayaleneh@gmail.com>
2018-10-24 06:05:27 -0400

    modifing javascript file

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ function handler(){
   let name;
  name= prompt("hello can you enter your name?");
  alert("hello "+name+ " you can put any answer that you want");
- let m_name=prompt("Do you remeber my name if so write it?"+name);
- if(m_name==="ayaleneh")
+ let m_name=prompt("Do you remeber my name if so write it? "+name);
+ if(m_name.toLocaleLowerCase=="ayaleneh")
  {
    alert("great job! go ahead for next question")
  }
@@ -11,7 +11,7 @@ function handler(){
    alert("you make amistake try the next");
  }
  let m_age=prompt("guess my age "+name);
- if(m_age===22){
+ if(m_age==22){
    alert("great..go ahead for the next question");
  }
  else{
